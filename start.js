27c6a9a58341c2c67fc2c879f92c5fbc075ba5fe
ernestovalent <ernesto.valen.t@gmail.com>
2021-07-08 19:04:22 -0500

    Mysql conexion

--- start.js
+++ start.js
@@ -1,14 +1,23 @@
-const db = require('./lib/cosmosdb/getDocument');
+const cosmos = require('./lib/cosmosdb/getDocument');
+const mysql = require('./lib/mysql/insertUsers');
 
 async function getGraphConfig(){
-  const promise = new Promise( (resolve, reject) => {
-    let getDocument = db.get();
+  let promise = new Promise( (resolve, reject) => {
+    let getDocument = cosmos.get();
     resolve(getDocument);
   });
   return promise;
 }
 
+async function insertMysql(){
+  let promise = new Promise( (resolve, reject) => {
+    let values = [
+      ['hey@xcaret.com', 'Caamalito','2021-06-12T17:31:23Z','2021-05-12T17:31:23Z'],
+      ['arnols@xcaret.com', 'Fers sita','2021-02-12T17:31:23Z','2021-05-12T17:31:23Z']]
+    let inserts = mysql.insertArray(values);
+    resolve(inserts);
+  });
+  return promise;
+}
 
-getGraphConfig()
-.then(document => console.log(document.resource));
-
+insertMysql();
