06f67ec2c0fab666abc8a3209d96701d734049f7
ernestovalent <ernesto.valen.t@gmail.com>
2021-07-13 18:39:48 -0500

    build with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+/** @type {import('webpack').Configuration} */
+
+module.exports = {
+  entry: './src/run.js',
+  target: 'node',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  node: {
+    __dirname: true,
+  },
+  optimization: {
+    minimize: false,
+  },
+   module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      parallel: true,
+      uglifyOptions: {
+        mangle: {
+          keep_fnames: true
+        }
+      }
+    }),
+  ]
+}
